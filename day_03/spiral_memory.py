f5ac008640dc9efc28e4b36151d1a9b0b2725627
Matjaž Finžgar <matjaz@finzgar.net>
2017-12-03 17:46:16 +0100

    Make the solution for day_03 a little prettier.

--- day_03/spiral_memory.py
+++ day_03/spiral_memory.py
@@ -19,37 +19,25 @@ def part_one(input_square):
         x, y = x + dx, y + dy
 
 
+def surrounding_positions(x, y):
+    return [(x+1, y), (x, y+1), (x+1, y+1), (x-1, y), (x, y-1), (x-1, y-1), (x-1, y+1), (x+1, y-1)]
+
+
 def part_two(last_square):
     x = y = dx = 0
     dy = -1
     coordinates = {(0, 0): 1}
 
     while True:
-        if coordinates[(x,y)] > last_square:
-            result = 0
-
-            for position in [(x+1, y), (x, y+1), (x+1, y+1), (x-1, y), (x, y-1), (x-1, y-1), (x-1, y+1), (x+1, y-1)]:
-                try:
-                    result += coordinates[position]
-                except Exception as e:
-                    pass
-                    
-            return result
-
         if (x == y) or (x < 0 and x == -y) or (x > 0 and x == 1 - y):
             dx, dy = -dy, dx
-        
-        x, y = x + dx, y + dy
-        
-        current_sum = 0
 
-        for position in [(x+1, y), (x, y+1), (x+1, y+1), (x-1, y), (x, y-1), (x-1, y-1), (x-1, y+1), (x+1, y-1)]:
-            try:
-                current_sum += coordinates[position]
-            except Exception as e:
-                pass
-        
-        coordinates[(x, y)] = current_sum
+        x, y = x + dx, y + dy
+
+        coordinates[(x, y)] = sum(coordinates[position] for position in surrounding_positions(x, y) if position in coordinates)
+
+        if coordinates[(x, y)] > last_square:
+            return coordinates[(x, y)]
 
 
 print(f'day_03: Solution for the first part is {part_one(puzzle_input)} and {part_two(puzzle_input)} for the second part.')
